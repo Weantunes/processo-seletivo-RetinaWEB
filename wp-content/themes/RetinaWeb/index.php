9e3cfc5e1f81d12f4f5d0254879ee25003cbb8f5
Wagner Eduardo de Souza Antunes <duzicoo@gmail.com>
2023-03-21 14:19:42 -0300

    .

---
+++ wp-content/themes/RetinaWeb/index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+
+<head>
+
+    <!--Autor-->
+    <meta name='author' content='Wagner Eduardo de Souza Antunes'>
+
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Processo seletivo - Retina WEB </title>
+
+    <!--Descrição do site-->
+    <meta name='description' content='Descrição desse site'>
+
+    <!--Inserção do Favicon-->
+    <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
+
+    <!--link CSS-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <!-- Inicio Header -->
+    <?php include 'Header.php'; ?>
+    <!-- FIM Header -->
+
+    <!--Ínicio Contúdo-->
+    <main>
+
+        <!--banner Principal-->
+        <section id="banner-MainMain" class="banner-main">
+            <div class="banners">
+                <div class="txtbanner2">
+                    <div class="txtbanner">
+
+                        <img src="assets/svg/title.svg" alt="">
+
+                        <p class="font9">Conheça como o Marketing Digital pode ajudar os <span class="sublinhado">médicos</span></p>
+                        <p class="font8">Decubra as melhores estratégias para contruir uma presença sólida uma presença
+                            solida na internet e atinja milhares de pacientes que procuram pela sua especialidade nesse
+                            exato momento</p>
+                        <a href="contato.php"><button class="button">Baixe gratuitamente o E-Book</button></a>
+                    </div>
+                    <div class="txtbanner">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Fim banner Principal-->
+
+        <!-- Ebook -->
+        <section id="ebook">
+            <div id="Serviços-container" class="container">
+
+                <div id="Texto-Foto">
+
+
+                    <div>
+                        <!--Título do texto-->
+                        <h2 class="font12">Não perca mais tempo! Aumente seu alcance e consiga reconhecimento em seu setor de atuação.</h2>
+                        <h3 class="font13">Como o Marketing Digital pode ajudar os Profissionais de Saúde</h3>
+
+                        <a href="" class="button">Baixe gratuitamente o E-Book</a>
+                    </div>
+
+                    <img src="assets/img/ebook.webp" alt="">
+
+                </div>
+            </div>
+        </section>
+        <!--Fim Ebook-->
+
+        <!-- Vantagens -->
+        <section id="Vantagens" class="banners">
+            <div id="Serviços-container" class="container">
+
+                <div id="Texto-Foto">
+
+                    <!--Título do texto-->
+                    <h2 class="font12">Vantagens do Marketing Digital para Médicos</h2>
+
+                    <div>
+                        <div class="carousel">
+                            <ul class="carousel-list">
+                                <li class="carousel-item active">
+                                    <article class="ItemVantengens">
+                                        <span>
+                                            <img src="assets/svg/divulgacao-etica.svg" alt="">
+
+                                            <h3 class="font12">Divulgação Ética</h3>
+                                        </span>
+
+                                        <p class="font14">Mesmo que muitas redes sociais façam uso de imagens e a Resolução 1.0974/11 no artigo 3º proíba a exposição de pacientes para divulgação de técnicas ou de sua marca, isso não faz com que essa plataforma seja ineficiente para a divulgação de seus serviços.
+                                        </p>
+                                        <br>
+                                        <p class="font14">Posts informativos e institucionais que coloquem o médico como autoridade no assunto dentro do meio online, fundamentando-se num bom planejamento de marketing afim de angariar novos seguidores nas redes e aumentar o engajamento e posicionamento da marca com estratégias de Marketing Digital.</p>
+                                        <a href="" class="button">Fale Conosco</a>
+
+                                    </article>
+                                </li>
+                                <li class="carousel-item">
+                                    <article class="ItemVantengens">
+                                        <span>
+                                            <img src="assets/svg/divulgacao-etica.svg" alt="">
+
+                                            <h3 class="font12">Divulgação Ética</h3>
+                                        </span>
+
+                                        <p class="font14">Mesmo que muitas redes sociais façam uso de imagens e a Resolução 1.0974/11 no artigo 3º proíba a exposição de pacientes para divulgação de técnicas ou de sua marca, isso não faz com que essa plataforma seja ineficiente para a divulgação de seus serviços.
+                                        </p>
+                                        <br>
+                                        <p class="font14">Posts informativos e institucionais que coloquem o médico como autoridade no assunto dentro do meio online, fundamentando-se num bom planejamento de marketing afim de angariar novos seguidores nas redes e aumentar o engajamento e posicionamento da marca com estratégias de Marketing Digital.</p>
+                                        <a href="" class="button">Fale Conosco</a>
+
+                                    </article>
+                                </li>
+                                <li class="carousel-item">
+                                    <article class="ItemVantengens">
+                                        <span>
+                                            <img src="assets/svg/divulgacao-etica.svg" alt="">
+
+                                            <h3 class="font12">Divulgação Ética</h3>
+                                        </span>
+
+                                        <p class="font14">Mesmo que muitas redes sociais façam uso de imagens e a Resolução 1.0974/11 no artigo 3º proíba a exposição de pacientes para divulgação de técnicas ou de sua marca, isso não faz com que essa plataforma seja ineficiente para a divulgação de seus serviços.
+                                        </p>
+                                        <br>
+                                        <p class="font14">Posts informativos e institucionais que coloquem o médico como autoridade no assunto dentro do meio online, fundamentando-se num bom planejamento de marketing afim de angariar novos seguidores nas redes e aumentar o engajamento e posicionamento da marca com estratégias de Marketing Digital.</p>
+                                        <a href="" class="button">Fale Conosco</a>
+
+                                    </article>
+                                </li>
+                            </ul>
+                            <button class="carousel-control prev">Anterior</button>
+                            <button class="carousel-control next">Próximo</button>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--Fim Vantagens-->
+
+        <!--Conheça a Retina-->
+        <section id="SobreNos" class="banners">
+            <div id="Serviços-container" class="container">
+
+                <div id="Texto-Foto">
+
+                    <img src="assets/img/sobre-retina.webp" alt="">
+
+                    <div>
+                        <!--Título do texto-->
+                        <h2 class="font11">Conheça a Retina</h2>
+                        <h3 class="font4">Somos uma agência web criativa que <br> valoriza o seu projeto!</h3>
+
+                        <!--Texto-->
+                        <article>
+
+                            <p class="font10">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ad blanditiis, quis illum
+                                esse repellendus cum facere possimus officiis quos id porro eum vero. Perferendis
+                                quisquam deserunt provident et fugit.
+                            </p>
+                            <p class="font10">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ad blanditiis, quis illum
+                                esse repellendus cum facere possimus officiis quos id porro eum vero. Perferendis
+                                quisquam deserunt provident et fugit. Quo, aliquam. Dolor ipsam placeat possimus labore
+                                doloribus est culpa error.
+                            </p>
+                            <p class="font10">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ad blanditiis, quis illum
+                                esse repellendus cum facere possimus officiis quos id porro eum vero.
+                            </p>
+
+                            <a href="" class="button">Fale Conosco</a>
+
+                        </article>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!--Fim Conheça a Retina-->
+
+        <!-- Clientes -->
+        <section id="Clientes" class="banners">
+            <div id="Serviços-container" class="container">
+
+                <div id="Texto-Foto">
+
+
+
+                    <div>
+                        <!--Título do texto-->
+                        <h2 class="font12">Clientes que confiam <br>
+                            no nosso trabalho</h2>
+                        <h3 class="font14">Sabemos a importância da experiência do cliente para os negócios e, portanto,nós nos esforçamos para fazer sua marca se destacar no mercado.</h3>
+
+                        <ul>
+                            <!-- <li><img src="assets/svg/arrow-esquerda.svg" alt=""></li>
+                            <li><img src="assets/svg/arrow-direita.svg" alt=""></li> -->
+                        </ul>
+
+                    </div>
+
+                    <div>
+
+                        <div>
+
+                        </div>
+
+                        <!--Texto-->
+                        <article>
+
+                            <span>
+                                <img src="" alt="">
+                                <span>
+                                    <img src="" alt="">
+                                    <img src="" alt="">
+                                    <img src="" alt="">
+                                    <img src="" alt="">
+                                    <img src="" alt="">
+                                </span>
+                            </span>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus a inventore doloribus, aliquam ex officiis aperiam, vitae hic autem vel consequuntur corrupti fuga consectetur. Rem tempora eos eius illo dolore.</p>
+
+                        </article>
+                    </div>
+
+                    <a href="" class="button">Fale Conosco</a>
+
+                </div>
+            </div>
+        </section>
+        <!--Fim Clientes-->
+
+
+    </main>
+    <!--Fim Contúdo-->
+
+    <!-- Inicio Footer -->
+    <?php include 'footer.php'; ?>
+    <!-- FIM Footer -->
+
+</body>
+
+</html>
